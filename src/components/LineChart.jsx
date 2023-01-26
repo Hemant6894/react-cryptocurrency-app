2a981afa62ac57af4c5efb45b75b8a976f26e7ed
Hemant6894 <hemant.pasricha94@gmail.com>
2023-01-26 18:22:04 +0530

    added code

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,58 +1,76 @@
-
 import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 import { Line } from 'react-chartjs-2';
 import { Col, Row, Typography } from 'antd';
 
-const { Title } = Typography;
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-const LineChart = ({ coinHistory, currentPrice, coinName }) => {
-  const coinPrice = [];
-  const coinTimestamp = [];
 
-  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-    coinPrice.push(coinHistory?.data?.history[i].price);
-  }
+export default function LineChart({ coinHistory, currentPrice, coinName }) {
+    const coinPrice = [];
+    const coinTimestamp = [];
+  
+    for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+      coinPrice.push(coinHistory?.data?.history[i].price);
+    }
+  
+    for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+      coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
+    }
 
-  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-    coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
-  }
-  const data = {
-    labels: coinTimestamp,
-    datasets: [
-      {
-        label: 'Price In USD',
-        data: coinPrice,
-        fill: false,
-        backgroundColor: '#0071bd',
-        borderColor: '#0071bd',
-      },
-    ],
-  };
+    console.log(coinTimestamp, "time")
+    const data = {
+        labels: coinTimestamp,
+        datasets: [
+          {
+            label: 'Price In USD',
+            data: coinPrice,
+            fill: false,
+            backgroundColor: '#0071bd',
+            borderColor: '#0071bd',
+          },
+        ],
+      };
 
-  const options = {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true,
+      const options = {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: `${coinName} Price Chart`,
           },
         },
-      ],
-    },
-  };
+      };
 
-  return (
-    <>
-      <Row className="chart-header">
-        <Title level={2} className="chart-title">{coinName} Price Chart </Title>
-        <Col className="price-container">
-          <Title level={5} className="price-change">Change: {coinHistory?.data?.change}%</Title>
-          <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
-        </Col>
+  return( 
+    <div>
+    <Row className="chart-header">
+    <Col className="price-container">
+    <h4 level={5} className="price-change">Change: {coinHistory?.data?.change}%</h4>
+    <h4 level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</h4>
+  </Col>
       </Row>
-      <Line data={data} options={options} />
-    </>
-  );
-};
-
-export default LineChart;
+    <Line options={options} data={data} />
+    </div>
+    );
+}
